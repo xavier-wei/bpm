3e1287bee38cf386aa11e4e58aea3ff50c2a7616
Chienlin <2301021@iisigroup.com>
2023-07-06 16:26:51 +0800

    modify webpack context path

--- bpm/webpack/webpack.common.js
+++ bpm/webpack/webpack.common.js
@@ -40,7 +40,7 @@ module.exports = async (env, options) => {
         port: 9060,
         proxy: [
           {
-            context: ['/api', '/services', '/management', '/v3/api-docs', '/h2-console', '/auth'],
+            context: ['/bpm','/api', '/services', '/management', '/v3/api-docs', '/h2-console', '/auth'],
             target: 'http://localhost:8080',
             secure: false,
           },
