70544d5b315309bf39e0906066d03b88945ee6cb
Chienlin <2301021@iisigroup.com>
2023-07-07 16:02:16 +0800

    add flwoable and L414.bpmn20.xml

---
+++ flowable/src/test/java/tw/gov/pcc/flowable/FlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.gov.pcc.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
