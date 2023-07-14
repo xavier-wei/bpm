08d2d1082be3d41ea3142dad4ec5baec68a75881
Chienlin <2301021@iisigroup.com>
2023-07-14 16:20:48 +0800

    modify TaskDTO.java definition, add judge process status function.

--- flowable/src/main/java/tw/gov/pcc/flowable/domain/TaskDTO.java
+++ flowable/src/main/java/tw/gov/pcc/flowable/domain/TaskDTO.java
@@ -11,13 +11,13 @@ public class TaskDTO {
     private String taskId; // 任務ID
     private String taskName; // 任務名稱
     private String createdTime; // 任務創建時間
-
-    public TaskDTO(Task task) {
+    private boolean isProcessComplete; // 判斷此任務完成後流程是否結束
+    public TaskDTO(Task task,Boolean isProcessComplete) {
         this.taskId = task.getId();
         this.taskName = task.getName();
         this.processInstanceId = task.getProcessInstanceId();
         this.createdTime = CovertTimeZone.convertTimezoneTaipei(task.getCreateTime());
-
+        this.isProcessComplete = isProcessComplete;
     }
 
 }
