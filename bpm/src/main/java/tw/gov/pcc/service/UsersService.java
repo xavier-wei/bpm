80e3011813233587009dbb2c0598812b9c3296ab
2301021 <cl.chia@iisigroup.com>
2023-08-10 08:00:49 +0800

    修改相關sql、增加User entity

---
+++ bpm/src/main/java/tw/gov/pcc/service/UsersService.java
@@ -0,0 +1,22 @@
+package tw.gov.pcc.service;
+
+import org.springframework.stereotype.Service;
+import tw.gov.pcc.repository.UsersRspoitory;
+
+@Service
+public class UsersService {
+
+//   private final Logger log = LoggerFactory.getLogger(UsersService.class);
+    private UsersRspoitory usersRspoitory;
+
+    public UsersService(UsersRspoitory usersRspoitory) {
+        this.usersRspoitory = usersRspoitory;
+    }
+
+
+    public String findFirstByUserId(String userId) {
+        return usersRspoitory.findFirstByUserId(userId).getUserId();
+    }
+
+
+}
