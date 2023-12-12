e2599184aae1aed83737d010894ef1ba814f828d
2301021 <cl.chia@iisigroup.com>
2023-12-12 13:22:19 +0800

    修改格式 判斷是否需要寄信

--- bpm/src/main/java/tw/gov/pcc/service/MailService.java
+++ bpm/src/main/java/tw/gov/pcc/service/MailService.java
@@ -18,7 +18,7 @@ public class MailService {
     private final String mailTitle = "ISMS表單處理完畢通知：%s({%s},{%s}) %s)"; // 表單名稱，申請人，申請人ID，狀態
     private final String mailContent = "%s \n" +
         "表單單號: %s \n" +
-        "申請人：{ %s}, { %s}\n" +
+        "申請人：{%s}, { %s}\n" +
         "狀態：  %s，詳情請參閱表單查詢之簽核狀態\n" ; // 表單名稱，表單編號，申請人，申請人ID，狀態
 
     public MailService(EipMailDataRepository eipMailDataRepository, UserService userService, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -28,13 +28,17 @@ public class MailService {
     }
 
     public void sendMail(MailInfo mailInfo) {
-        User userInfo = userService.getUserInfo(mailInfo.getApplierId());
-        EipMailData eipMailData = new EipMailData();
-        eipMailData.setMailId(getNewMailId());
-        eipMailData.setEmail(userInfo.getEmail());
-        eipMailData.setSubject(String.format(mailTitle, mailInfo.getFormName(), mailInfo.getApplier(), mailInfo.getApplierId(), mailInfo.getStatus()));
-        eipMailData.setMessage(String.format(mailContent, mailInfo.getFormName(), mailInfo.getFormId(), mailInfo.getApplier(), mailInfo.getApplierId(), mailInfo.getStatus()));
-        eipMailDataRepository.save(eipMailData);
+        if (mailInfo.getIsSend()) {
+            User userInfo = userService.getUserInfo(mailInfo.getApplierId());
+            EipMailData eipMailData = new EipMailData();
+            eipMailData.setMailId(getNewMailId());
+            eipMailData.setEmail(userInfo.getEmail());
+            eipMailData.setIsMailed("E");
+            eipMailData.setFilePath("");
+            eipMailData.setSubject(String.format(mailTitle, mailInfo.getFormName(), mailInfo.getApplier(), mailInfo.getApplierId(), mailInfo.getStatus()));
+            eipMailData.setMessage(String.format(mailContent, mailInfo.getFormName(), mailInfo.getFormId(), mailInfo.getApplier(), mailInfo.getApplierId(), mailInfo.getStatus()));
+            eipMailDataRepository.save(eipMailData);
+        }
     }
 
     public String getNewMailId() {
