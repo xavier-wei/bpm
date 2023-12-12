24617b1a9a269c7055342d503c2613d51c1fb442
2301021 <cl.chia@iisigroup.com>
2023-12-12 14:54:20 +0800

    修改格式 判斷是否需要寄信

--- bpm/src/main/java/tw/gov/pcc/service/MailService.java
+++ bpm/src/main/java/tw/gov/pcc/service/MailService.java
@@ -15,10 +15,10 @@ public class MailService {
     private final EipMailDataRepository eipMailDataRepository;
     private final UserService userService;
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    private final String mailTitle = "ISMS表單處理完畢通知：%s({%s},{%s}) %s)"; // 表單名稱，申請人，申請人ID，狀態
+    private final String mailTitle = "ISMS表單處理完畢通知：%s( %s, %s) %s)"; // 表單名稱，申請人，申請人ID，狀態
     private final String mailContent = "%s \n" +
         "表單單號: %s \n" +
-        "申請人：{%s}, { %s}\n" +
+        "申請人： %s, %s\n" +
         "狀態：  %s，詳情請參閱表單查詢之簽核狀態\n" ; // 表單名稱，表單編號，申請人，申請人ID，狀態
 
     public MailService(EipMailDataRepository eipMailDataRepository, UserService userService, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
@@ -33,8 +33,9 @@ public class MailService {
             EipMailData eipMailData = new EipMailData();
             eipMailData.setMailId(getNewMailId());
             eipMailData.setEmail(userInfo.getEmail());
-            eipMailData.setIsMailed("E");
+            eipMailData.setIsMailed("");
             eipMailData.setFilePath("");
+            eipMailData.setAttachFileName("");
             eipMailData.setSubject(String.format(mailTitle, mailInfo.getFormName(), mailInfo.getApplier(), mailInfo.getApplierId(), mailInfo.getStatus()));
             eipMailData.setMessage(String.format(mailContent, mailInfo.getFormName(), mailInfo.getFormId(), mailInfo.getApplier(), mailInfo.getApplierId(), mailInfo.getStatus()));
             eipMailDataRepository.save(eipMailData);
